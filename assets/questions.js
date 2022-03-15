cb2e765eef548ddeaaa43d5358a379027cba1bc6
Grace <helen.g.anderson@me.com>
2022-03-15 23:26:09 +1100

    add files

---
+++ assets/questions.js
@@ -0,0 +1,112 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "3. alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "parentheses", "curly brackets", "square brackets"],
+    answer: "2. parentheses",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "All of the above",
+    ],
+    answer: "4. All of the above",
+  },
+  {
+    title: "String values must be enclosed within ____.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "3. quotes",
+  },
+  {
+    title:
+      "A useful tool used during development and debugging for printing content to the debugger is ____.",
+    choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "4. console.log",
+  },
+  {
+    title: "Which of the following is correct about features of JavaScript?",
+    choices: [
+      "Javascript integrates with HTML",
+      "Javascript is cross-platform",
+      "JavaScript is open",
+      "All of the above",
+    ],
+    answer: "4. All of the above",
+  },
+  {
+    title: "What is the correct syntax to print a page using JavaScript?",
+    choices: [
+      "window.print()",
+      "browser.print()",
+      "navigator.print()",
+      "document.print()",
+    ],
+    answer: "1. window.print()",
+  },
+  {
+    title:
+      "Which built-in method returns the calling string value converted to upper case?",
+    choices: [
+      "toUpperCase()",
+      "toUpper()",
+      "changeCase(case)",
+      "None of the above",
+    ],
+    answer: "1. toUpperCase()",
+  },
+  {
+    title:
+      "Which built-in method removes the last element from an array and returns that element?",
+    choices: ["last()", "get()", "pop()", "None of the above"],
+    answer: "3. pop()",
+  },
+  {
+    title:
+      "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+    choices: ["toSource()", "valueOf()", "toString()", "None of the above"],
+    answer: "1. toSource()",
+  },
+  {
+    title:
+      "Which of the following function of String object returns the characters in a string between two indexes into the string?",
+    choices: ["slice()", "split()", "substr()", "substring()"],
+    answer: "4. substring()",
+  },
+  {
+    title:
+      "Which of the following function of String object returns the primitive value of the specified object?",
+    choices: [
+      "toLocaleUpperCase()",
+      "toUpperCase()",
+      "toString()",
+      "valueOf()",
+    ],
+    answer: "4. valueOf()",
+  },
+  {
+    title:
+      "Which of the following function of String object causes a string to be displayed as a superscript, as if it were in a <sup> tag?",
+    choices: ["sup()", "small()", "strike()", "sub()"],
+    answer: "1. sup()",
+  },
+  {
+    title:
+      "Which of the following function of Array object creates a new array with all of the elements of this array for which the provided filtering function returns true?",
+    choices: ["concat()", "every()", "filter()", "some()"],
+    answer: "3. filter()",
+  },
+  {
+    title:
+      "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
+    choices: ["unshift()", "sort()", "splice()", "toString()"],
+    answer: "1. unshift()",
+  },
+];
+//quiz questions from assignment and https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
---
+++ assets/script.js
@@ -0,0 +1,286 @@
+// element selectors
+var navBar = document.querySelector("nav");
+var highScoresLink = document.getElementById("highscores-link");
+var timerDisplay = document.getElementById("timer");
+var container = document.getElementById("container");
+var title = document.getElementById("title");
+var text = document.getElementById("text");
+var startQuizButton = document.getElementById("start-button");
+var quizAnswers = document.getElementById("answers");
+var answerChoice = document.getElementsByClassName("answer-choice");
+var answerMessage = document.getElementById("answer-message");
+var input = document.getElementById("input");
+var initials = document.getElementById("initials");
+var submitInitialsButton = document.getElementById("submit-initials-button");
+var submitForm = document.getElementById("submit-form");
+
+// states
+var timer = 0;
+var currentQuestion = 0;
+var score = 0;
+var scoreArray = [];
+var timerInterval = false;
+
+// functions
+
+//1. startQuiz called by startQuizButton
+//2. startQuiz updates timer to 75,
+//hides startQuizButton
+//calls countdown -> 3
+//calls displayQuestion -> 4
+function startQuiz() {
+  timer = 75;
+  timerDisplay.textContent = timer;
+
+  countdown();
+
+  displayQuestion();
+
+  startQuizButton.style.display = "none";
+}
+
+//3. countdown sets timer interval to 1second
+// starts decrementing the timer by 1, displays decrementing timer
+function countdown() {
+  timerInterval = setInterval(function () {
+    timer--;
+    timerDisplay.textContent = timer;
+    //if timer value is less than one, display zero
+    // and end the game endGame -> 7.
+    // and clear the timerInterval countdown
+    if (timer < 1) {
+      timerDisplay.textContent = 0;
+      endGame();
+      clearInterval(timerInterval);
+    }
+    //if currentQuestion equals length of questions array, stop the timer decrementing
+    if (currentQuestion === questions.length) {
+      // timerDisplay.textContent = timer;
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+//4. displayQuestion function (called by startQuiz)
+function displayQuestion() {
+  //styles for quiz-page
+  container.className = "quiz-page p-3 mb-2 bg-light text-dark";
+  //set text of question to be currentQuestion + 1
+  title.textContent = "Question " + (currentQuestion + 1);
+  title.setAttribute("class", "h2");
+  //call the currentQuestion index's text from the questions array (questions.js file)
+  text.textContent = questions[currentQuestion].title;
+  text.className = "h4";
+  //update text style for questions
+  text.setAttribute(
+    "style",
+    "border-top: 1px double #adb5bd; padding-top: 2rem;"
+  );
+  //display the quizAnswers
+  //update answers div to display answer choices called from questions array
+  quizAnswers.style.display = "block";
+
+  answerChoice[0].textContent = "1. " + questions[currentQuestion].choices[0];
+  answerChoice[1].textContent = "2. " + questions[currentQuestion].choices[1];
+  answerChoice[2].textContent = "3. " + questions[currentQuestion].choices[2];
+  answerChoice[3].textContent = "4. " + questions[currentQuestion].choices[3];
+  //add event listener to each answerChoice.
+  //when an answerChoice text is clicked, call checkAnswer -> 5.
+  for (i = 0; i < answerChoice.length; i++) {
+    answerChoice[i].addEventListener("click", checkAnswer);
+  }
+}
+
+//5. checkAnswer (called by displayQuestion)
+//pass answerChoice click event to checkAnswer function
+function checkAnswer(event) {
+  //if the clicked answerChoice equals the answer text in the questions array
+  if (event.target.textContent === questions[currentQuestion].answer) {
+    //display the answer-message-correct message
+    answerMessage.style.display = "block";
+    answerMessage.textContent = "Correct!";
+    answerMessage.className = "answer-message-correct";
+    //add one to currentQuestion variable
+    currentQuestion++;
+    //add one to the score
+    score++;
+    //hide the correct answer message after 1 second
+    setTimeout(function () {
+      answerMessage.style.display = "none";
+    }, 1000);
+    //if currentQuestion = questions.length, endGame -> 6.
+    if (currentQuestion === questions.length) {
+      endGame();
+      //else continue to displayQuestion -> 4.
+    } else {
+      displayQuestion();
+    }
+    //else the answer is incorrect, so
+  } else {
+    //display the answer-message-incorrect message
+    answerMessage.style.display = "block";
+    answerMessage.textContent = "Incorrect!";
+    answerMessage.className = "answer-message-incorrect";
+    //add one to currentQuestion variable
+    currentQuestion++;
+    //hide the incorrect answer message after 1 second
+    setTimeout(function () {
+      answerMessage.style.display = "none";
+    }, 1000);
+    //if timer is less than 10, subtract 10 then endGame -> 6.
+    if (timer < 10) {
+      timer -= 10;
+      // timer === 0;
+      endGame();
+      // else if currentQuestion is the last question then endGame -> 6.
+      // } else if currentQuestion >= 14) {
+    } else if (currentQuestion === questions.length) {
+      endGame();
+    } else {
+      //else reduce timer by 10 and go to displayQuestion -> 4.
+      timer -= 10;
+      displayQuestion();
+    }
+  }
+}
+
+//6. function endGame - display the results page with score and message
+function endGame() {
+  //hide the quizAnswers display
+  quizAnswers.style.display = "none";
+  //styles for result display
+  container.className = "results-page p-3 mb-2 bg-light text-dark";
+  title.setAttribute("class", "h2");
+  text.setAttribute("style", "border-top: 0");
+  text.removeAttribute("class");
+  //display score variable
+  text.textContent = `Your final score is ${score}.`;
+  input.style.display = "block";
+
+  if (timer <= 0) {
+    //if the timer is 0 or less, set timer to 0 and display out of time message
+    timer === 0;
+    title.textContent = "You ran out of time!";
+  } else {
+    //if timer is > 0 display all done message
+    title.textContent = "All done!";
+  }
+  // click on Submit button to store score
+  // then call storeScore function -> 7.
+  // submitInitialsButton.addEventListener("click", storeScore);
+
+  submitForm.addEventListener("submit", storeScore) 
+
+}
+
+//7. storeScore
+// save the scores locally, and order from highest to lowest score
+// trigger storeScore from submitInitialsButton click event
+function storeScore(event) {
+  //prevent submitting button from refreshing page
+  event.preventDefault();
+
+  //test for text being entered into the input
+  if (initials.value.length === 0) {
+    return;
+  } else {
+    //if there is input text, add user's initials input to the score variable as an instance of userScore object
+    var userScore = {
+      userName: initials.value.trim(),
+      userScore: score,
+    };
+    //add userScore object instance to scoreArray
+    scoreArray.push(userScore);
+    //use compare function to sort scoreArray in descending userScores
+    scoreArray.sort((a, b) => b.userScore - a.userScore);
+    //save updated scoreArray in local storage
+    localStorage.setItem("score", JSON.stringify(scoreArray));
+
+    openHighScores();
+  }
+}
+
+// 8. openHighScores
+//called after initials and scores are saved locally
+//OR by clicking High Scores link
+function openHighScores() {
+  // clear the timer decrimenting
+  if (timerInterval) {
+    clearInterval(timerInterval);
+  }
+  //set up score-page for display
+  container.className = "score-page p-3 mb-2 bg-light text-dark";
+  var ul = document.createElement("ul");
+  //"Go Back" button
+  var goBackButton = document.createElement("button");
+  goBackButton.setAttribute("class", "btn btn-primary btn-lg");
+  goBackButton.textContent = "Go Back";
+  //"Clear High Scores" button
+  var clearHighScoresButton = document.createElement("button");
+  clearHighScoresButton.setAttribute("class", "btn btn-primary btn-lg");
+  clearHighScoresButton.textContent = "Clear High Scores";
+  //append elements to the container
+  container.appendChild(ul);
+  container.appendChild(goBackButton);
+  container.appendChild(clearHighScoresButton);
+  //hide startQuizButton
+  startQuizButton.style.display = "none";
+  //hide navbar
+  navBar.style.visibility = "hidden";
+  //title and content display
+  title.textContent = "High Scores";
+  text.textContent = "";
+  text.setAttribute("style", "border-top: 0");
+  //hide quiz answers
+  quizAnswers.style.display = "none";
+  input.style.display = "none";
+
+  //loop through score array to create score string to display
+  for (i = 0; i < scoreArray.length; i++) {
+    var scoreText =
+      "Initials: " +
+      scoreArray[i].userName +
+      " - Score: " +
+      scoreArray[i].userScore;
+
+    //add string to li, append li to the ul
+    li = document.createElement("li");
+    li.textContent = scoreText;
+    ul.appendChild(li);
+  }
+  //clicking "Go Back", refreshes index.html to open
+  goBackButton.addEventListener("click", function () {
+    location.href = "index.html";
+  });
+  //clicking "Clear High Scores", clears local storage and displays ul with no content
+  clearHighScoresButton.addEventListener("click", function () {
+    localStorage.clear();
+    ul.innerHTML = "";
+  });
+}
+
+//1.b loadHighScore
+//load locally saved high scores into scoreArray when user opens/refreshes index page - ready to add to from quiz game
+function loadHighScore() {
+  //use JSON to call and parse score stored locally
+  //"score" is updated and saved locally by 7. storeScore
+  var storedScores = JSON.parse(localStorage.getItem("score"));
+
+  //if there are locally stored scores, assign storedScores to scoreArray
+  if (storedScores !== null) {
+    scoreArray = storedScores;
+
+    return scoreArray;
+  }
+}
+
+// load / refresh page or initiate by clicking link / button
+
+//1.a. loadHighScore is called when index.html refreshed/opened -> 1.b
+loadHighScore();
+
+//clicking highScoresLink -> go to openHighScores -> 8.
+highScoresLink.addEventListener("click", openHighScores);
+
+//1. Clicking startQuizButton ("Start Quiz") triggers startQuiz function -> 2.
+startQuizButton.addEventListener("click", startQuiz);
